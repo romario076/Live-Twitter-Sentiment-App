a28d164e878001d17a1d491550f97bd4f1df497f
Roman Melnyk <romario076@yahoo.com>
2019-01-06 14:57:45 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,9 @@
 
 class RunConfig:
-    ckey="7Q26tbTPi6W0HhNT93t1yBZI4"
-    csecret="rHzSrzXiVKvTBeYxmcQv8b5fUHvW0SGvbHgJKlIHXwvKNdgjdM"
-    atoken="181176256-tTWjmvyVkeOdX5UpsE5xvQAsq98au2gDWNXn9VHK"
-    asecret="HgFNRhrL4jCGzoMN5JOyGaMry9T5nn1ity5HAN0mcPfg9"
+    ckey="your-ckey"
+    csecret="your-csecret"
+    atoken="your-atoken"
+    asecret="your-asecret"
     dbNameInit = 'twitter.db'
     tableName = 'Tweets'
     positiveNegativeThreshold = 0.001
